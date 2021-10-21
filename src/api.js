9c2b7892dbf26876245593f9742b96f6f544c598
Carlos Eduardo <cadutaborda@gmail.com>
2021-10-21 19:49:48 -0300

    Biblioteca de comunicação com a API adicionada

---
+++ src/api.js
@@ -0,0 +1,23 @@
+import axios from 'axios' 
+
+const api = axios.create({
+    baseURL: 'https://pokedex20201.herokuapp.com/' 
+})
+
+export const getPokemon = (pokemon) =>  api.get('pokemons/' + pokemon)
+export const getPage = (page) => api.get('pokemons?page=' + page.toString())
+
+export const createUser = (username) => {
+    api.post('users', {
+      username: username.toString()
+    })
+    .then(function (response){
+    })
+    .catch(function (err){
+        console.log('usuario ja existente')
+    })
+}
+
+export const getUser = (username) => api.get('users/' + username)
+export const createFav = (username, pokemon) => api.post('users/' + username + '/starred/' + pokemon)
+export const deleteFav = (username, pokemon) => api.delete('users/' + username + '/starred/' + pokemon)
