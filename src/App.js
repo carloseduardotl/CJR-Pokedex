2f38dbb349a8ed8741024d7a5c08ca4bbf8c2b52
Carlos Eduardo <cadutaborda@gmail.com>
2021-10-24 16:13:14 -0300

    Pagina home criada e testes da api adicioados

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import * as api from './Components/api';
 
 function App() {
+
+
+
+
+/*     api.getUser('cadu1')
+    .then(function (response){
+        console.log(response.data)
+    }) */
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
--- src/Components/home.js
+++ src/Components/home.js
@@ -1,1 +1,41 @@
 import * as api from './api.js'
+
+
+function Home(){
+
+    function pokemon(pokemon, user){
+        api.getPokemon('ghdsk')
+        .then(function (response){
+            console.log(response.data);
+        })
+        .catch(function (err){
+            console.log(err)
+        })
+    }
+
+    function removeFav(pokemon, user){
+        api.deleteFav('cadu1', 'pikachu')
+        .then(function (response){
+            //console.log(response.data);
+        })
+        .catch(function (err){
+            console.log(err)
+        })
+    }
+
+    function cadastro(user){
+        api.createUser(user)
+    }
+
+    return (
+        <div className="App">
+        <header className="App-header">
+          <button onClick = {() => pokemon('pikachu', 'cadu1')}> pikachuuuu </button> 
+          <button onClick = {() => removeFav('pikachu', 'cadu1')}> delete </button> 
+          <button onClick = {() => cadastro('cadu3')}> criar usuário </button> 
+        </header>
+      </div>
+    )
+}
+
+export default Home;
